4222db5e955cbf1f9702cca3e39a857a0dc58b79
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2024-03-26 06:16:08 +0100

    Create a test file

---
+++ src/js/storage/save.test.js
@@ -0,0 +1,29 @@
+import { save } from "./save.js";
+
+global.localStorage = {
+  setItem: jest.fn((key, value) => {
+    global.localStorage[key] = value;
+  }),
+  getItem: jest.fn((key) => {
+    return global.localStorage[key];
+  }),
+  removeItem: jest.fn((key) => {
+    delete global.localStorage[key];
+  }),
+};
+
+describe("saveToLocal", () => {
+  it("save a value to local storage", () => {
+    const theKey = "A key";
+    const theValue = 123;
+
+    save(theKey, theValue);
+
+    expect(localStorage[theKey]).toEqual(JSON.stringify(theValue));
+    expect(localStorage.setItem).toHaveBeenCalledWith(
+      theKey,
+      JSON.stringify(theValue),
+    );
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+  });
+});
