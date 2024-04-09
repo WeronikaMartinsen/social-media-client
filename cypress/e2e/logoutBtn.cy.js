b9cb1c395bca243bd3ae107c425258f00ca26448
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2024-04-09 10:30:33 +0200

    Updates logout test

--- cypress/e2e/logoutBtn.cy.js
+++ cypress/e2e/logoutBtn.cy.js
@@ -1,29 +1,18 @@
 describe("Logout function", () => {
-  it("Logs out the user and clears the browser storage", () => {
-    cy.window().then((win) => {
-      win.localStorage.setItem("token", "exampleToken")
-    })
+  it("Logs in the user and log out.", () => {
+    cy.visit("https://weronikamartinsen.github.io/social-media-client/")
+    cy.wait(1000)
 
-    cy.visit("/?view=profile&name=weronikaMartin")
+    cy.get('.modal-footer button[data-auth="login"]')
+      .should("be.visible")
+      .click()
 
-    cy.window().its("localStorage.token").should("equal", "exampleToken")
-
-    cy.get("button#logOut").click()
-
-    cy.window().its("localStorage.token").should("not.exist")
-  })
-
-  it("Logs out the user and clears the browser storage with different profile name", () => {
-    cy.window().then((win) => {
-      win.localStorage.setItem("token", "exampleToken")
-    })
-
-    cy.visit("/?view=profile&name=weronikaMartin")
-
-    cy.window().its("localStorage.token").should("equal", "exampleToken")
-
-    cy.get("button#logOut").click()
-
-    cy.window().its("localStorage.token").should("not.exist")
+    cy.get("#loginEmail").invoke("val", "maryb@noroff.no")
+    cy.get("#loginPassword").invoke("val", "marybnoroff")
+    cy.wait(500)
+    cy.get("#loginForm button").contains("Login").click()
+    cy.wait(1000)
+    cy.get('button[data-auth="logout"]').click()
+    cy.get('.modal-footer button[data-auth="login"]').should("exist")
   })
 })
