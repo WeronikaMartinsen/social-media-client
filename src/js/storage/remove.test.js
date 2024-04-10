d094d25168ceb6070444c1a852380975157238ec
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2024-04-10 20:58:11 +0200

    Format prettier

--- src/js/storage/remove.test.js
+++ src/js/storage/remove.test.js
@@ -1,18 +1,18 @@
-import { remove } from "./remove.js";
+import { remove } from "./remove.js"
 
 global.localStorage = {
   removeItem: jest.fn((key) => {
-    global.localStorage[key];
+    global.localStorage[key]
   }),
-};
+}
 
 describe("remove", () => {
   it("remove a value from local storage", () => {
-    const theKey = "A key";
+    const theKey = "A key"
 
-    remove(theKey);
+    remove(theKey)
 
-    expect(localStorage.removeItem).toHaveBeenCalledWith(theKey);
-    expect(localStorage.removeItem).toHaveBeenCalledTimes(1);
-  });
-});
+    expect(localStorage.removeItem).toHaveBeenCalledWith(theKey)
+    expect(localStorage.removeItem).toHaveBeenCalledTimes(1)
+  })
+})
--- src/js/storage/save.test.js
+++ src/js/storage/save.test.js
@@ -1,29 +1,29 @@
-import { save } from "./save.js";
+import { save } from "./save.js"
 
 global.localStorage = {
   setItem: jest.fn((key, value) => {
-    global.localStorage[key] = value;
+    global.localStorage[key] = value
   }),
   getItem: jest.fn((key) => {
-    return global.localStorage[key];
+    return global.localStorage[key]
   }),
   removeItem: jest.fn((key) => {
-    delete global.localStorage[key];
+    delete global.localStorage[key]
   }),
-};
+}
 
 describe("save", () => {
   it("save a value to local storage", () => {
-    const theKey = "A key";
-    const theValue = 123;
+    const theKey = "A key"
+    const theValue = 123
 
-    save(theKey, theValue);
+    save(theKey, theValue)
 
-    expect(localStorage[theKey]).toEqual(JSON.stringify(theValue));
+    expect(localStorage[theKey]).toEqual(JSON.stringify(theValue))
     expect(localStorage.setItem).toHaveBeenCalledWith(
       theKey,
       JSON.stringify(theValue),
-    );
-    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
-  });
-});
+    )
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1)
+  })
+})
